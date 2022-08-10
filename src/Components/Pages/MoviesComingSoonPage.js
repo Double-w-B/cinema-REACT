091563345934f58b152f84dd2c5febfe1c838a62
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-08-10 22:03:45 +0200

    added structure and a logic for a component

--- src/Components/Pages/MoviesComingSoonPage.js
+++ src/Components/Pages/MoviesComingSoonPage.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { StyledMoviesContainer } from "../Movies/MoviesNowPlaying";
+import SingleMoviePoster from "../Movies/SingleMoviePoster";
+import Navigation from "../Navigation";
 
 const MoviesComingSoonPage = () => {
-  return <main>MoviesComingSoonPage</main>;
-}
+  const { moviesComingSoon } = useSelector((store) => store.movies);
+  console.log(moviesComingSoon);
+  return (
+    <StyledMainContainer>
+      <Navigation pageTitle={"Coming Soon"} />
+      <StyledMoviesContainer>
+        {moviesComingSoon.slice(4).map((movie) => {
+          return (
+            <SingleMoviePoster
+              key={movie.id}
+              movieInfo={movie}
+              pageTitle={"Coming Soon"}
+            />
+          );
+        })}
+      </StyledMoviesContainer>
+    </StyledMainContainer>
+  );
+};
+
+const StyledMainContainer = styled.main`
+  width: 70%;
+  margin: 2rem auto;
+`;
 
 export default MoviesComingSoonPage;
