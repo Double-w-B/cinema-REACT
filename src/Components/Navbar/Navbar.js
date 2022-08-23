86937e6321faab03b94cfbc3c2fca4da024760c3
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-08-23 23:55:39 +0200

    added an authentication logic to a Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,14 +4,28 @@ import { Link, useLocation } from "react-router-dom";
 import logoImg from "../../Images/Logo.png";
 import { VscAccount } from "react-icons/vsc";
 import { navbarLinks } from "../../data";
+import { useAuth0 } from "@auth0/auth0-react";
+import spinnerImg from "../../../src/Images/spinner_2.gif";
 
 const Navbar = (props) => {
   const location = useLocation();
+  const {
+    loginWithRedirect,
+    isAuthenticated,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
+
+  const isUser = isAuthenticated && user;
 
   const handleClick = () => {
     setTimeout(() => {
       props.nowPlayingContainer.current.scrollIntoView({ behavior: "smooth" });
     }, 400);
+  };
+  const logoutUser = () => {
+    logout({ returnTo: window.location.origin });
   };
 
   const activeLink = (path) => {
@@ -23,6 +37,7 @@ const Navbar = (props) => {
     else return "#fff";
   };
 
+  console.log({ isAuthenticated, user, isLoading });
   return (
     <header>
       <StyledLogoContainer>
@@ -53,21 +68,44 @@ const Navbar = (props) => {
         </nav>
       </StyledNavContainer>
       <StyledLogInContainer>
-        <VscAccount /> <p>Log in</p>
+        {isUser && !isLoading ? (
+          <div className="isLogged">
+            <img src={user.picture} alt={user.given_name} />
+            <div>
+              <p>
+                Hi,{" "}
+                <strong>
+                  {user.given_name ? user.given_name : user.name.split(" ")[0]}
+                </strong>
+              </p>
+              <button onClick={logoutUser}>Log out</button>
+            </div>
+          </div>
+        ) : isLoading ? (
+          <div className="isLogged">
+            <img src={spinnerImg} alt="spinner" />
+          </div>
+        ) : (
+          <>
+            <VscAccount />
+            <button onClick={loginWithRedirect}>Log in</button>
+          </>
+        )}
       </StyledLogInContainer>
     </header>
   );
 };
 
 const StyledLogoContainer = styled.div`
-  width: 15%;
+  width: 20%;
   height: 100%;
+  padding-right: 2rem;
   display: grid;
   align-content: center;
   justify-items: end;
 
   a {
-    width: 40%;
+    width: 80px;
     height: 100%;
     display: grid;
     place-items: center;
@@ -76,6 +114,7 @@ const StyledLogoContainer = styled.div`
       width: 100%;
       height: 100%;
       object-fit: contain;
+      display: block;
       transition: all 0.3s linear;
       color: transparent;
 
@@ -87,37 +126,80 @@ const StyledLogoContainer = styled.div`
 `;
 
 const StyledLogInContainer = styled.div`
-  width: 15%;
+  width: 20%;
   height: 100%;
+  padding-left: 1rem;
   display: flex;
   align-items: center;
   justify-content: flex-start;
   font-size: 1.2rem;
-  color: #fff;
+  color: var(--primary-grey-clr);
 
-  *:first-child {
+  svg {
+    font-size: 1.6rem;
     margin-right: 0.5rem;
-    font-size: 1.5rem;
+    transition: all 0.2s linear;
+    color: var(--primary-grey-clr);
+
+    &:hover + button {
+      color: var(--primary-white-clr);
+    }
   }
 
-  p {
+  button {
+    font-size: 1.2rem;
+    border: none;
+    color: var(--primary-grey-clr);
+    background-color: transparent;
     transition: all 0.2s linear;
+    cursor: pointer;
+
     &:hover {
-      cursor: pointer;
-      color: #f12535;
+      color: var(--primary-white-clr);
+    }
+    &:active {
+      transform: scale(0.8);
+    }
+  }
+
+  .isLogged {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 45px;
+      height: 45px;
+      margin-right: 0.5rem;
+      color: transparent;
+      border-radius: 50%;
+      border: 1px solid var(--primary-grey-clr);
+      box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    }
+
+    div {
+      position: relative;
+      color: var(--primary-white-clr);
+      button {
+        font-size: 1rem;
+        position: absolute;
+        bottom: -1.5rem;
+        right: 0;
+      }
     }
   }
 `;
 
 const StyledNavContainer = styled.div`
-  width: 85%;
+  width: 60%;
   height: 100%;
   display: grid;
   place-items: center;
-  color: #fff;
+  color: var(--primary-white-clr);
 
   nav {
-    width: 80%;
+    width: 100%;
     height: 90%;
 
     ul {
@@ -129,7 +211,7 @@ const StyledNavContainer = styled.div`
 
       li {
         border: none;
-        border-left: 1px solid #fff;
+        border-left: 1px solid var(--primary-white-clr);
         text-transform: capitalize;
         display: grid;
         place-items: center;
@@ -138,17 +220,17 @@ const StyledNavContainer = styled.div`
 
         a {
           font-size: 1.1rem;
-          color: #fff;
+          color: var(--primary-white-clr);
           letter-spacing: 0.5px;
           transition: all 0.1s linear;
 
           &:hover {
-            color: #f12535 !important;
+            color: var(--primary-red-clr) !important;
           }
         }
 
         &:last-child {
-          border-right: 1px solid #fff;
+          border-right: 1px solid var(--primary-white-clr);
         }
       }
     }
