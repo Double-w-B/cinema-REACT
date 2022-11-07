b9e4f22bab174ffbee416a9889421fd31909580b
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-11-07 22:33:17 +0100

    changed the code structure to improve the UI

--- src/Components/Pages/Booking/Summary/SummaryContent.js
+++ src/Components/Pages/Booking/Summary/SummaryContent.js
@@ -49,40 +49,42 @@ const SummaryContent = (props) => {
       shakeSeatsAndTickets={props.isSeatsEqTicketsAmount}
     >
       <h2>{bookingMovieTitle}</h2>
-      <p>
-        <span>Tickets:</span>{" "}
-        {allTickets ? allTickets + ` (${numberOfTickets()})` : allTickets}
-      </p>
-      <p>
-        <span>Date:</span> {bookingDay}
-      </p>
-      <p>
-        <span>Time:</span> {bookingTime}
-      </p>
 
-      <p>
-        <span>Seats:</span> {bookingSeats.join(", ")}
-      </p>
-      <p>
-        <span>Total:</span> ${bookingTotalPrice}
-      </p>
-      {(!isUser || !storedUserEmail) && (
-        <label>
-          Email address:{" "}
-          <input
-            type="email"
-            required
-            value={props.userEmail || ""}
-            onChange={(e) => props.setUserEmail(e.target.value.toLowerCase())}
-          />
-        </label>
-      )}
+      <div className="info">
+        <div className="info__labels">
+          <p>Date:</p>
+          <p>Time:</p>
+          <p>Seats:</p>
+          <p>Total:</p>
+          <p>Tickets:</p>
+          {(!isUser || !storedUserEmail) && <p>Email:</p>}
+        </div>
+
+        <div className="info__details">
+          <p>{bookingDay}</p>
+          <p>{bookingTime ? bookingTime : "-"}</p>
+          <p>{bookingSeats.length > 0 ? bookingSeats.join(", ") : "-"}</p>
+          <p>${bookingTotalPrice}</p>
+          <p>
+            {allTickets ? allTickets + ` (${numberOfTickets()})` : allTickets}
+          </p>
+          {(!isUser || !storedUserEmail) && (
+            <input
+              type="email"
+              required
+              value={props.userEmail || ""}
+              onChange={(e) => props.setUserEmail(e.target.value.toLowerCase())}
+            />
+          )}
+        </div>
+      </div>
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.div`
   width: 45%;
+  min-width: 450px;
   height: 100%;
   padding: 0 0 0.5rem 0.5rem;
   display: flex;
@@ -93,54 +95,76 @@ const StyledContainer = styled.div`
     margin-bottom: 1rem;
   }
 
-  p {
+  .info {
+    width: 100%;
+    height: calc(100% - 32px);
     font-size: 1.1rem;
-    &:nth-child(2),
-    &:nth-child(5) {
-      -webkit-animation: ${(props) => props.shakeSeatsAndTickets && shake};
-      -moz-animation: ${(props) => props.shakeSeatsAndTickets && shake};
-      -o-animation: ${(props) => props.shakeSeatsAndTickets && shake};
-      animation: ${(props) => props.shakeSeatsAndTickets && shake};
-      animation-duration: 5.72s;
+    display: flex;
+
+    .info__labels,
+    .info__details {
+      width: 100px;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      p {
+        color: var(--primary-grey-clr);
+      }
     }
 
-    span {
-      color: var(--primary-grey-clr);
+    .info__details {
+      width: 250px;
+      padding-left: 0.5rem;
+
+      p {
+        color: var(--primary-white-clr);
+      }
+
+      input {
+        width: 100%;
+        border: none;
+        outline: none;
+        padding-left: 0.5rem;
+        font-size: 1.05rem;
+        border-radius: 2px;
+        text-transform: lowercase;
+        background-color: transparent;
+        border: ${(props) =>
+          /^[^ ]+@[^ ]+\.[a-z]{2,3}$/.test(props.userEmail)
+            ? "1px solid transparent"
+            : "1px solid var(--primary-grey-clr)"};
+        color: var(--primary-white-clr);
+
+        &:hover {
+          border: 1px solid var(--primary-white-clr);
+        }
+        &:focus {
+          opacity: 1;
+          border: 1px solid var(--primary-white-clr);
+        }
+      }
     }
   }
 
-  label {
-    color: var(--primary-grey-clr);
+  .info__labels p:last-child,
+  .info__details input {
     -webkit-animation: ${(props) => props.shakeEmail && shake};
     -moz-animation: ${(props) => props.shakeEmail && shake};
     -o-animation: ${(props) => props.shakeEmail && shake};
     animation: ${(props) => props.shakeEmail && shake};
     animation-duration: 5.72s;
-
-    input {
-      width: 60%;
-      border: none;
-      outline: none;
-      padding-left: 0.5rem;
-      font-size: 1.05rem;
-      border-radius: 2px;
-      text-transform: lowercase;
-      transition: 0.3s linear;
-      background-color: transparent;
-      border: ${(props) =>
-        /^[^ ]+@[^ ]+\.[a-z]{2,3}$/.test(props.userEmail)
-          ? "1px solid transparent"
-          : "1px solid var(--primary-grey-clr)"};
-      color: var(--primary-white-clr);
-
-      &:hover {
-        border: 1px solid var(--primary-white-clr);
-      }
-      &:focus {
-        opacity: 1;
-        border: 1px solid var(--primary-white-clr);
-      }
-    }
+  }
+  .info__labels p:nth-child(3),
+  .info__labels p:nth-child(5),
+  .info__details p:nth-child(3),
+  .info__details p:nth-child(5) {
+    -webkit-animation: ${(props) => props.shakeSeatsAndTickets && shake};
+    -moz-animation: ${(props) => props.shakeSeatsAndTickets && shake};
+    -o-animation: ${(props) => props.shakeSeatsAndTickets && shake};
+    animation: ${(props) => props.shakeSeatsAndTickets && shake};
+    animation-duration: 5.72s;
   }
 `;
 
