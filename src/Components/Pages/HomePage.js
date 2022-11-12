cf7b16e501f50ec29dd319fcbaa0115045785690
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2022-11-12 23:58:23 +0100

    added loading spinner before data being fetched

--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -2,11 +2,19 @@ import React from "react";
 import MoviesNowPlayingSlider from "../Sliders/MoviesNowPlayingSlider";
 import MoviesComingSoonSlider from "../Sliders/MoviesComingSoonSlider";
 import MoviesNowPlaying from "../Movies/MoviesNowPlaying";
+import { useSelector } from "react-redux";
+import LoadingPage from "./LoadingPage/LoadingPage";
 
 const HomePage = (props) => {
+  const { nowPlayingIsLoading } = useSelector((store) => store.movies);
+
   React.useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  if (nowPlayingIsLoading) {
+    return <LoadingPage />;
+  }
 
   return (
     <main>
